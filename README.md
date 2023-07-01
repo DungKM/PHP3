840322e24ee664493859b3083e3aceb9414ffe2d
Hoàng Anh Dũng <108377940+DungKM@users.noreply.github.com>
2023-07-01 11:18:36 +0700

    example 1 database

--- README.md
+++ README.md
@@ -64,3 +64,11 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+# Database 
+php artisan make:migration create_products_table
+
+- rollback
+php artisan migrate:rollback --step=
+- Update 
+php artisan make:migration update_name_column_users_table --table=users
---
+++ database/migrations/2023_07_01_031645_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Brand;
+use App\Models\Category;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('gender');
+            $table->float('price', 8, 2);
+            $table->string('image')->nullable();
+            $table->text('description');
+            $table->unsignedInteger('brand_id');
+            $table->unsignedInteger('category_id');
+ 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
